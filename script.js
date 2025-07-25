260041980a998ed1b524d55e04aebe5c90c7ff64
Yousef El-Maghrabi <yousefmaghrabi2009@gmail.com>
2025-07-25 14:22:41 +0300

    Updated the  script.js

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-const text = [
-  "Web & Mobile Developer",
-  "React & Flutter Expert",
-  "UI/UX Designer",
-  "Content Creator"
-];
+window.onload = function () {
+  const text = [
+    "Web & Mobile Developer",
+    "React & Flutter Expert",
+    "UI/UX Designer",
+    "Content Creator"
+  ];
 
-let count = 0;
-let index = 0;
-let currentText = '';
-let letter = '';
+  let count = 0;
+  let index = 0;
+  let currentText = '';
+  let letter = '';
+  const typingElement = document.querySelector('.typing');
 
-(function typeEffect() {
-  if (count === text.length) count = 0;
-  currentText = text[count];
-  letter = currentText.slice(0, ++index);
+  function typeEffect() {
+    if (!typingElement) return;
+    if (count === text.length) count = 0;
+    currentText = text[count];
+    letter = currentText.slice(0, ++index);
+    typingElement.textContent = letter;
 
-  document.querySelector('.typing').textContent = letter;
-
-  if (letter.length === currentText.length) {
-    count++;
-    index = 0;
-    setTimeout(typeEffect, 2000);
-  } else {
-    setTimeout(typeEffect, 100);
+    if (letter.length === currentText.length) {
+      count++;
+      index = 0;
+      setTimeout(typeEffect, 1500);
+    } else {
+      setTimeout(typeEffect, 100);
+    }
   }
-})();
+
+  typeEffect();
+};
