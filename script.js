6621e9497aeafe61430d04d538f8f036eeb3ce2b
Yousef El-Maghrabi <yousefmaghrabi2009@gmail.com>
2025-07-25 14:18:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-function toggleDarkMode() {
-  document.body.classList.toggle('dark-mode');
-}
+const text = [
+  "Web & Mobile Developer",
+  "React & Flutter Expert",
+  "UI/UX Designer",
+  "Content Creator"
+];
+
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function typeEffect() {
+  if (count === text.length) count = 0;
+  currentText = text[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector('.typing').textContent = letter;
+
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(typeEffect, 2000);
+  } else {
+    setTimeout(typeEffect, 100);
+  }
+})();
